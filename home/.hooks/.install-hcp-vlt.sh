3aeda73a94e1d9bb5a43e028e1ad670a0e8ebe5a
Rowan Gillson <devops@the.bald.engineer>
2023-12-03 16:53:06 +1000

    chore: remove output logging from init hook script

--- home/.hooks/.install-hcp-vlt.sh
+++ home/.hooks/.install-hcp-vlt.sh
@@ -8,14 +8,11 @@ bin_directory="${HOME}/.local/bin"
 # Create $HOME/bin directory if missing and add it to PATH
 setup_bin_directory() {
   if [ ! -d "$bin_directory" ]; then
-    printf "Info: Creating %s directory...\n" "$bin_directory"
     mkdir -p "$bin_directory"
-    printf "Info: Setting permissions on %s...\n" "$bin_directory"
     chmod 0755 "$bin_directory"
   fi
 
   if [[ ":$PATH:" != *":$bin_directory:"* ]]; then
-    printf "Info: Adding %s to PATH...\n" "$bin_directory"
     # Prefer appending to PATH so existing binaries from Homebrew, etc. are found first
     export PATH="${PATH}:${bin_directory}"
   fi
@@ -25,10 +22,8 @@ setup_bin_directory() {
 check_binary_installed() {
   local binary_name=$1
   if command -v "$binary_name" >/dev/null 2>&1; then
-    printf "Info: %s binary already installed. Continuing...\n" "$binary_name"
     return 0
   else
-    printf "Warning: %s binary not found in PATH. Installing...\n" "$binary_name"
     return 1
   fi
 }
@@ -37,15 +32,9 @@ check_binary_installed() {
 fetch_binary() {
   local binary_name=$1
   local download_url=$2
-  printf "Info: Fetching %s binary...\n" "$binary_name"
-  printf "Info: Download URL: %s\n" "$download_url"
-
   if ! curl -sL "$download_url" -o "${bin_directory}/${binary_name}"; then
-    printf "Error: Failed to download %s binary from %s\n" "$binary_name" "$download_url"
     exit 1
   fi
-
-  printf "Info: Setting executable permissions on %s/%s...\n" "$bin_directory" "$binary_name"
   chmod +x "${bin_directory}/${binary_name}"
 }
 
@@ -70,19 +59,15 @@ fetch_vlt_binary() {
   vlt_download_url=$(curl -s https://releases.hashicorp.com/vlt/index.json | jq -r --arg os "$vlt_system_os" --arg arch "$vlt_system_arch" '.versions | to_entries | map(select(.key | test("^[0-9]+\\.[0-9]+\\.[0-9]+$"))) | map(.value.builds[] | select(.os == $os and .arch == $arch)) | sort_by(.version) | last | .url') || exit 1
 
   check_binary_installed "python3" || {
-    printf "Error: Python3 not found in PATH. Exiting!\n"
     exit 1
   }
 
   # Check if vlt is already installed and skip if found, otherwise install it
   if check_binary_installed "vlt"; then
-    # printf "Info: vlt already installed...\n"
     return 0
   else
     fetch_binary "vlt.zip" "$vlt_download_url"
-    printf "Info: Unzipping vlt.zip to %s...\n" "$bin_directory"
     python3 -c "import zipfile, os; zipfile.ZipFile('${bin_directory}/vlt.zip').extractall('${bin_directory}'); os.chmod('${bin_directory}/vlt', 0o755); os.remove('${bin_directory}/vlt.zip')" || {
-      printf "Error: Failed to unzip vlt.zip to %s. Exiting!\n" "$bin_directory"
       exit 1
     }
   fi
@@ -92,7 +77,6 @@ main() {
   setup_bin_directory
   fetch_jq_binary
   fetch_vlt_binary
-  printf "Info: Completed init hook script for Chezmoi dependencies.\n"
 }
 
 main "$@"
