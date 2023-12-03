961da4b566a6cb3e5e14667ff146f35674de72e1
Rowan Gillson <devops@the.bald.engineer>
2023-12-03 13:43:50 +1000

    feat: add support for Homebrew on Linux

---
+++ home/.chezmoiscripts/linux/controlled/run_once_before_10_linux-install-homebrew.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# This script installs Homebrew if it is not yet installed on the target system. It relies
+# on the following environment variable set in the Chezmoi config: BREW_PATH
+
+# Check for Homebrew
+if ! command -v brew &> /dev/null; then
+    printf "Info: Homebrew is not installed. Installing Homebrew...\n"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    printf "Info: Homebrew is already installed. Continuing...\n"
+fi
+
+# Show Homebrew version
+BREW_VERSION="$(${BREW_PATH} --version)"
+printf "Info: Found %s installed.\n" "${BREW_VERSION}"
