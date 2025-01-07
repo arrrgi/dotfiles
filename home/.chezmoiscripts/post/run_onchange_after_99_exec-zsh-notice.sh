2ee02ecec701fcb50c68fba0af41263c45b151ad
Rowan Gillson <devops@the.bald.engineer>
2025-01-07 20:04:40 +1000

    fix: fix binary path order and zsh notice script (#53)
    - update zshrc with re-order paths putting Homebrew before OS
    - rename zsh reload notice script
    - add hash inclusion to noticify when zshrc changes

---
+++ home/.chezmoiscripts/post/run_onchange_after_99_exec-zsh-notice.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: (c) 2024 Rowan Gillson <devops@the.bald.engineer>
+# SPDX-License-Identifier: MIT
+#
+# .zshrc hash: {{ include (joinPath .chezmoi.sourceDir "dot_config/zsh/dot_zshrc.tmpl") | sha256sum }}
+set -euo pipefail
+# Display an ANSI escape code notice that changes to Zsh have been made and
+# the user should exec zsh to apply them.
+printf '\033[1;33m%s\033[0m\n' 'Warning: Changes to Zsh config have been made. Run "exec zsh" to apply them.'
