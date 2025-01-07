7d0772df1665f580b8024f170a972d8bfcce4cae
Rowan Gillson <devops@the.bald.engineer>
2025-01-07 19:59:51 +1000

    fix: fix binary path order and zsh notice script
    - update zshrc with re-order paths putting Homebrew before OS
    - rename zsh reload notice script
    - add hash inclusion to noticify when zshrc changes

--- home/.chezmoiscripts/post/run_after_99_exec-zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# SPDX-FileCopyrightText: (c) 2024 Rowan Gillson <devops@the.bald.engineer>
-# SPDX-License-Identifier: MIT
-#
-
-set -euo pipefail
-printf '%s\n' "Reloading shell to apply changes..."
-exec zsh
