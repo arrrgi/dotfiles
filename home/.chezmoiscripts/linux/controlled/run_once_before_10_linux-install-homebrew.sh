f2f3cad6a0a474b3db66587800ab6b5e28f7b18e
Rowan Gillson <devops@the.bald.engineer>
2025-05-01 08:14:48 +1000

    feat: update packages and project tools (#72)

--- home/.chezmoiscripts/linux/controlled/run_once_before_10_linux-install-homebrew.sh
+++ home/.chezmoiscripts/linux/controlled/run_once_before_10_linux-install-homebrew.sh
@@ -9,12 +9,24 @@ set -euo pipefail
 # This script installs Homebrew if it is not yet installed on the target system. It relies
 # on the following environment variable set in the Chezmoi config: BREW_PATH
 
+# Check that required Linux packages are installed as a pre-requisite
+# for Homebrew installation: build-essential procps curl file git
+REQUIRED_PACKAGES=("build-essential" "procps" "curl" "file" "git")
+for package in "${REQUIRED_PACKAGES[@]}"; do
+		if ! dpkg -s "${package}" &> /dev/null; then
+				printf "Info: %s is not installed. Installing...\n" "${package}"
+				sudo apt-get install -y "${package}"
+		else
+				printf "Info: %s is already installed.\n" "${package}"
+		fi
+done
+
 # Check for Homebrew
 if ! command -v brew &> /dev/null; then
-    printf "Info: Homebrew is not installed. Installing Homebrew...\n"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		printf "Info: Homebrew is not installed. Installing Homebrew...\n"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-    printf "Info: Homebrew is already installed. Continuing...\n"
+		printf "Info: Homebrew is already installed. Continuing...\n"
 fi
 
 # Show Homebrew version
